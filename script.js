79ab637de10401f2e9af13093715fe1b9cfb71e7
Sameem <nsam603@gmail.com>
2025-04-09 17:19:37 +0530

    na

--- script.js
+++ script.js
@@ -12,11 +12,40 @@ const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${
 const data = await response.json();
 console.log(data); 
 
-
+let definitions = data[0].meanings[0].definitions[0]
 resultDiv.innerHTML = `
-<p>${data[0].meanings[0].definitions[0].definition}</p>
-<p>${data[0].meanings[0].definitions[0].definition}</p>
-<p>${data[0].meanings[0].definitions[0].definition}</p> 
 
-`
+<h2> <strong>Word: </strong>${data[0].word}</h2>
+<p>${data[0].meanings[0].partOfSpeech[0]}</p>
+
+<p><strong>Meaning: </strong>${definitions.definition === undefined ? "Not Found" : definitions.definition}</p> 
+
+ <p><strong>Example: </strong>${definitions.example === undefined ? "Not Found" : 
+definitions.example}</p>
+
+
+<p><strong>Antonyms</strong></p>
+
+
+`;
+
+if(definitions.antonyms.length === 0 ){
+ resultDiv.innerHTML+= `<span>Not Found</span>`
+}
+else{
+for(let i = 0 ; i<definitions.antonyms.length; i++){
+ 
+  resultDiv.innerHTML += `<li>${definitions.antonyms[i]}</li>`
+}}
+
+
+for(let i = 0; i<definitions.synonyms.length; i++){
+  
+  resultDiv.innerHTML += `<li> ${definitions.synonyms[i]}</li>`
+}
+
+
+ 
+
+
 }
